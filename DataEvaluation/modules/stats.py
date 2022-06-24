66d51b69d28cb3c6a302dd4a879de313949cd042
Jonas Mucke <jonas.mucke@web.de>
2022-06-24 11:55:33 +0200

    nearly final cleanup

--- DataEvaluation/modules/stats.py
+++ DataEvaluation/modules/stats.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from cliffs_delta import cliffs_delta
 import scipy.stats as stats
 
 def mannwhitneyu_for_df(df, class_column, value_column, alpha = 0.05):
@@ -16,15 +17,25 @@ def mannwhitneyu_for_df(df, class_column, value_column, alpha = 0.05):
     return df_tmp
 
 def effectsize_for_df(df, class_column, value_column):
-    df_effect_size = pd.DataFrame([], columns=["Categorie_A", "Categorie_B", "Effect_Size"])
+    df_effect_size = pd.DataFrame([], columns=["Categorie_A", "Categorie_B", "Effect_Size", "Effect_Size_Interpretation"])
     styles = df[class_column].unique()
     for idx, style_a in enumerate(styles):
         for style_b in styles[idx + 1:]:
-            style_a_data = df[df[class_column] == style_a][value_column]
-            style_b_data = df[df[class_column] == style_b][value_column]
-            effect_size = cohend(style_a_data, style_b_data)
-            df_effect_size.loc[len(df_effect_size)] = [style_a, style_b, effect_size]
+            style_a_data = df[df[class_column] == style_a][value_column].values
+            style_b_data = df[df[class_column] == style_b][value_column].values
+            effect_size, result = cliffs_delta(style_a_data, style_b_data)
+            df_effect_size.loc[len(df_effect_size)] = [style_a, style_b, effect_size, result]
     return df_effect_size
+
+def correlations_for_df(df, factor, columns, value):
+    df_correlation = pd.DataFrame(columns=[factor, "PreExperience", "Correlation"])
+    for column in columns:
+        for style in df[factor].unique():
+            df_sub = df[df[factor] == style]
+            df_corr = df_sub[[value, column]].corr()
+            coef = df_corr[value].values[1]
+            df_correlation.loc[len(df_correlation)] = [style, column, coef]
+    return df_correlation
 
 def cohend(d1, d2) -> float:
     # calculate the size of samples
